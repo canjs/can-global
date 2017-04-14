78745a64ac6113305d717b67b1f1d88199eb2460
imaustink <austin@austinkurpuis.com>
2017-04-14 10:25:33 -0700

    1.0.0

---
+++ dist/amd/can-global.js
@@ -0,0 +1,18 @@
+/*can-global@0.0.0#can-global*/
+define(function (require, exports, module) {
+    (function (global) {
+        var GLOBAL;
+        module.exports = function (setGlobal) {
+            if (setGlobal !== undefined) {
+                GLOBAL = setGlobal;
+            }
+            if (GLOBAL) {
+                return GLOBAL;
+            } else {
+                return GLOBAL = typeof WorkerGlobalScope !== 'undefined' && self instanceof WorkerGlobalScope ? self : typeof process === 'object' && {}.toString.call(process) === '[object process]' ? global : window;
+            }
+        };
+    }(function () {
+        return this;
+    }()));
+});
